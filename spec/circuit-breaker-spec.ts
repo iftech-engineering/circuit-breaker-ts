d6122d5e98a35198b960d67cf67681488f4f79fe
themez <xian.zeng@ruguoapp.com>
2019-04-17 16:59:35 +0800

    promisify `breaker.run` interface

--- spec/circuit-breaker-spec.ts
+++ spec/circuit-breaker-spec.ts
@@ -5,28 +5,27 @@ describe('CircuitBreaker', function () {
   let breaker: CircuitBreaker
 
   const success = function () {
-    const command = function (success: Function) {
-      success()
-    }
+    const command = () => null
 
-    breaker.run(command)
+    return breaker.run(command).catch(() => {})
   }
 
   const fail = function () {
-    const command = function (success: Function, failed: Function) {
-      failed()
+    const command = () => {
+      throw 'fail'
     }
 
-    breaker.run(command)
+    return breaker.run(command).catch(() => {})
   }
 
   const timeout = function () {
     const command = function () { }
-    breaker.run(command)
-
+    const ret = breaker.run(command).catch(() => {})
+    
     jasmine.clock().tick(1000)
     jasmine.clock().tick(1000)
     jasmine.clock().tick(1000)
+    return ret
   }
 
   beforeEach(function () {
@@ -40,37 +39,46 @@ describe('CircuitBreaker', function () {
 
   describe('with a working service', function () {
 
-    it('should run the command', function () {
+    it('should run the command', async function () {
       const command = jasmine.createSpy()
-      breaker.run(command)
+      await breaker.run(command)
 
       expect(command).toHaveBeenCalled()
     })
 
-    it('should be able to notify the breaker if the command was successful', function () {
-      success()
+    it('should be able to notify the breaker if the command was successful', async function () {
+      await success()
 
       const bucket = breaker._lastBucket()
       expect(bucket.successes).toBe(1)
     })
 
-    it('should be able to notify the breaker if the command failed', function () {
-      fail()
+    it('should be able to notify the breaker if the command failed', async function () {
+      await fail()
 
       const bucket = breaker._lastBucket()
       expect(bucket.failures).toBe(1)
     })
 
-    it('should record a timeout if not a success or failure', function () {
-      timeout()
+    it('should record a timeout if not a success or failure', async function () {
+      await timeout()
 
       const bucket = breaker._buckets[breaker._buckets.length - 2]
       expect(bucket.timeouts).toBe(1)
     })
 
-    it('should not call timeout if there is a success', function () {
-      success()
+    it('should not call timeout if there is a success', async function () {
+      await success()
+      jasmine.clock().tick(1000)
+      jasmine.clock().tick(1000)
+      jasmine.clock().tick(1000)
+    
+      const bucket = breaker._lastBucket()
+      expect(bucket.timeouts).toBe(0)
+    })
 
+    it('should not call timeout if there is a failure', async function () {
+      await fail()
       jasmine.clock().tick(1000)
       jasmine.clock().tick(1000)
       jasmine.clock().tick(1000)
@@ -79,42 +87,48 @@ describe('CircuitBreaker', function () {
       expect(bucket.timeouts).toBe(0)
     })
 
-    it('should not call timeout if there is a failure', function () {
-      fail()
-
-      jasmine.clock().tick(1000)
-      jasmine.clock().tick(1000)
-      jasmine.clock().tick(1000)
-
-      const bucket = breaker._lastBucket()
-      expect(bucket.timeouts).toBe(0)
-    })
-
-    it('should not record a success when there is a timeout', function () {
-      const command = function (success: Function) {
-        jasmine.clock().tick(1000)
-        jasmine.clock().tick(1000)
-        jasmine.clock().tick(1000)
-
-        success()
+    it('should not record a success when there is a timeout', async function () {
+      const deferred = {} as { resolve: Function; reject: Function }
+      const promise = new Promise((resolve, reject) => {
+        deferred.resolve = resolve
+        deferred.reject = reject
+      })
+      const command = function() {
+        return promise
       }
-
-      breaker.run(command)
+      await Promise.all([
+        breaker.run(command).catch(() => {}),
+        void (function() {
+          jasmine.clock().tick(1000)
+          jasmine.clock().tick(1000)
+          jasmine.clock().tick(1000)
+          deferred.resolve()
+        })(),
+      ])
 
       const bucket = breaker._lastBucket()
       expect(bucket.successes).toBe(0)
     })
 
-    it('should not record a failure when there is a timeout', function () {
-      const command = function (success: Function, fail: Function) {
-        jasmine.clock().tick(1000)
-        jasmine.clock().tick(1000)
-        jasmine.clock().tick(1000)
+    it('should not record a failure when there is a timeout', async function () {
+      const deferred = {} as { resolve: Function; reject: Function }
+      const promise = new Promise((resolve, reject) => {
+        deferred.resolve = resolve
+        deferred.reject = reject
+      })
+      const command = function() {
+        return promise
+      }      
+      await Promise.all([
+        breaker.run(command).catch(() => {}),
+        void (function() {
+          jasmine.clock().tick(1000)
+          jasmine.clock().tick(1000)
+          jasmine.clock().tick(1000)
+          deferred.reject('fail')
+        })(),
+      ])
 
-        fail()
-      }
-
-      breaker.run(command)
 
       const bucket = breaker._lastBucket()
       expect(bucket.failures).toBe(0)
@@ -127,25 +141,25 @@ describe('CircuitBreaker', function () {
       spyOn(breaker, 'isOpen').and.returnValue(true)
     })
 
-    it('should not run the command', function () {
+    it('should not run the command', async function () {
       const command = jasmine.createSpy()
-      breaker.run(command)
+      await breaker.run(command).catch(() => {})
 
       expect(command).not.toHaveBeenCalled()
     })
 
-    it('should run the fallback if one is provided', function () {
+    it('should run the fallback if one is provided', async function () {
       const command = jasmine.createSpy()
       const fallback = jasmine.createSpy()
 
-      breaker.run(command, fallback)
+      await breaker.run(command).catch(fallback)
 
       expect(fallback).toHaveBeenCalled()
     })
 
-    it('should record a short circuit', function () {
+    it('should record a short circuit', async function () {
       const command = jasmine.createSpy()
-      breaker.run(command)
+      await breaker.run(command).catch(() => {})
 
       expect(command).not.toHaveBeenCalled()
 
@@ -156,91 +170,70 @@ describe('CircuitBreaker', function () {
 
   describe('isOpen', function () {
 
-    it('should be false if errors are below the threshold', function () {
+    it('should be false if errors are below the threshold', async function () {
       breaker.errorThreshold = 75
 
-      fail()
-      fail()
-      fail()
-      success()
+      await Promise.all([fail(), fail(), fail(), success()])
 
       expect(breaker.isOpen()).toBe(false)
     })
 
-    it('should be true if errors are above the threshold', function () {
+    it('should be true if errors are above the threshold', async function () {
       breaker.errorThreshold = 75
 
-      fail()
-      fail()
-      fail()
-      fail()
-      fail()
-      success()
+      await Promise.all([fail(), fail(), fail(), fail(), fail(), success()])
 
       expect(breaker.isOpen()).toBe(true)
     })
 
-    it('should be true if timeouts are above the threshold', function () {
+    it('should be true if timeouts are above the threshold', async function () {
       breaker.errorThreshold = 25
       breaker.volumeThreshold = 1
 
-      timeout()
-      timeout()
-      success()
+      await Promise.all([timeout(), timeout(), success()])
 
       expect(breaker.isOpen()).toBe(true)
     })
 
-    it('should maintain failed state after window has passed', function () {
+    it('should maintain failed state after window has passed', async function () {
       breaker.errorThreshold = 25
       breaker.volumeThreshold = 1
 
-      fail()
-      fail()
-      fail()
-      fail()
+      await Promise.all([fail(), fail(), fail(), fail()])
 
       jasmine.clock().tick(11001)
 
-      fail()
+      await fail()
 
       expect(breaker.isOpen()).toBe(true)
     })
 
-    it('should retry after window has elapsed', function () {
-      fail()
-      fail()
-      fail()
-      fail()
+    it('should retry after window has elapsed', async function () {
+      await Promise.all([fail(), fail(), fail(), fail()])
 
       jasmine.clock().tick(11001)
 
       const command = jasmine.createSpy()
-      breaker.run(command)
+      await breaker.run(command)
 
       expect(command).toHaveBeenCalled()
     })
 
-    it('should include errors within the current time window', function () {
+    it('should include errors within the current time window', async function () {
       breaker.errorThreshold = 75
 
-      fail()
-      fail()
-      fail()
-      fail()
-      fail()
-      success()
+      await Promise.all([fail(), fail(), fail(), fail(), fail(), success()])
 
       jasmine.clock().tick(1001)
 
       expect(breaker.isOpen()).toBe(true)
     })
 
-    it('should not be broken without having more than minumum number of errors', function () {
+    it('should not be broken without having more than minumum number of errors', async function () {
       breaker.errorThreshold = 25
       breaker.volumeThreshold = 1
 
-      fail()
+      await fail()
 
       expect(breaker.isOpen()).toBe(false)
     })
@@ -259,22 +252,17 @@ describe('CircuitBreaker', function () {
       breaker.onCircuitClose = closeSpy
     })
 
-    it('should call the onCircuitOpen method when a failure is recorded', function () {
-      fail()
-      fail()
-
+    it('should call the onCircuitOpen method when a failure is recorded', async function () {      
+      await Promise.all([fail(), fail()])
       expect(openSpy).toHaveBeenCalled()
     })
 
-    it('should call the onCircuitClosed method when the break is successfully reset', function () {
-      fail()
-      fail()
-      fail()
-      fail()
+    it('should call the onCircuitClosed method when the break is successfully reset', async function () {
+      await Promise.all([fail(), fail(), fail(), fail()])
 
       jasmine.clock().tick(11001)
 
-      success()
+      await success()
 
       expect(closeSpy).toHaveBeenCalled()
     })
@@ -282,40 +270,26 @@ describe('CircuitBreaker', function () {
 
   describe('forceClose', function () {
 
-    it('should bypass threshold checks', function () {
-      fail()
-      fail()
-      fail()
-      fail()
-      fail()
-      fail()
+    it('should bypass threshold checks', async function () {
+      await Promise.all([fail(), fail(), fail(), fail(), fail(), fail()])
 
       breaker.forceClose()
 
       const command = jasmine.createSpy()
-      breaker.run(command)
+      await breaker.run(command)
 
       expect(command).toHaveBeenCalled()
       expect(breaker.isOpen()).toBe(false)
     })
 
-    it('should not collect stats', function () {
-      fail()
-      fail()
-      fail()
-      fail()
-      fail()
-      fail()
+    it('should not collect stats', async function () {
+      await Promise.all([fail(), fail(), fail(), fail(), fail(), fail()])
 
       breaker.forceClose()
-      success()
-      success()
-      success()
-      success()
-      success()
+      await Promise.all([success(), success(), success(), success(), success()])
 
       const command = jasmine.createSpy()
-      breaker.run(command)
+      await breaker.run(command)
 
       expect(command).toHaveBeenCalled()
       expect(breaker.isOpen()).toBe(false)
@@ -324,40 +298,26 @@ describe('CircuitBreaker', function () {
 
   describe('forceOpen', function () {
 
-    it('should bypass threshold checks', function () {
-      success()
-      success()
-      success()
-      success()
-      success()
-      success()
+    it('should bypass threshold checks', async function () {
+      await Promise.all([success(), success(), success(), success(), success(), success()])
 
       breaker.forceOpen()
 
       const command = jasmine.createSpy()
-      breaker.run(command)
+      await breaker.run(command).catch(() => {})
 
       expect(command).not.toHaveBeenCalled()
       expect(breaker.isOpen()).toBe(true)
     })
 
-    it('should not collect stats', function () {
-      success()
-      success()
-      success()
-      success()
-      success()
-      success()
+    it('should not collect stats', async function () {
+      await Promise.all([success(), success(), success(), success(), success(), success()])
 
       breaker.forceOpen()
-      fail()
-      fail()
-      fail()
-      fail()
-      fail()
+      await Promise.all([fail(), fail(), fail(), fail(), fail()])
 
       const command = jasmine.createSpy()
-      breaker.run(command)
+      await breaker.run(command).catch(() => {})
 
       expect(command).not.toHaveBeenCalled()
       expect(breaker.isOpen()).toBe(true)
@@ -366,37 +326,27 @@ describe('CircuitBreaker', function () {
 
   describe('unforce', function () {
 
-    it('should recover from a force-closed circuit', function () {
-      fail()
-      fail()
-      fail()
-      fail()
-      fail()
-      fail()
+    it('should recover from a force-closed circuit', async function () {
+      await Promise.all([fail(), fail(), fail(), fail(), fail(), fail()])
 
       breaker.forceClose()
       breaker.unforce()
 
       const command = jasmine.createSpy()
-      breaker.run(command)
+      await breaker.run(command).catch(() => {})
 
       expect(command).not.toHaveBeenCalled()
       expect(breaker.isOpen()).toBe(true)
     })
 
-    it('should recover from a force-open circuit', function () {
-      success()
-      success()
-      success()
-      success()
-      success()
-      success()
+    it('should recover from a force-open circuit', async function () {
+      await Promise.all([success(), success(), success(), success(), success(), success()])
 
       breaker.forceOpen()
       breaker.unforce()
 
       const command = jasmine.createSpy()
-      breaker.run(command)
+      await breaker.run(command)
 
       expect(command).toHaveBeenCalled()
       expect(breaker.isOpen()).toBe(false)
